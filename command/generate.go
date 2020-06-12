22ffaf35a25e3fd1ff6d9eb8fd1e686448011cb4
Steven Landers <steven.landers@vonage.com>
2020-06-12 14:42:33 -0400

    hack out the password

--- command/generate.go
+++ command/generate.go
@@ -2,12 +2,11 @@ package command
 
 import (
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/yitsushi/go-commander"
 	"github.com/yitsushi/totp-cli/security"
-	s "github.com/yitsushi/totp-cli/storage"
-	"github.com/yitsushi/totp-cli/util"
 )
 
 // Generate structure is the representation of the generate command
@@ -16,25 +15,11 @@ type Generate struct {
 
 // Execute is the main function. It will be called on generate command
 func (c *Generate) Execute(opts *commander.CommandHelper) {
-	namespaceName := opts.Arg(0)
-	if len(namespaceName) < 1 {
-		panic("Namespace is not defined")
+	token := os.Getenv("TOTP_TOKEN")
+	if token == "" {
+		panic("TOTP_TOKEN not defined")
 	}
-
-	accountName := opts.Arg(1)
-	if len(accountName) < 1 {
-		panic("Account is not defined")
-	}
-
-	storage := s.PrepareStorage()
-
-	namespace, err := storage.FindNamespace(namespaceName)
-	util.Check(err)
-
-	account, err := namespace.FindAccount(accountName)
-	util.Check(err)
-
-	fmt.Println(security.GenerateOTPCode(account.Token, time.Now()))
+	fmt.Println(security.GenerateOTPCode(token, time.Now()))
 }
 
 // NewGenerate creates a new Generate command
@@ -44,8 +29,6 @@ func NewGenerate(appName string) *commander.CommandWrapper {
 		Help: &commander.CommandDescriptor{
 			Name:             "generate",
 			ShortDescription: "Generate a specific OTP",
-			Arguments:        "<namespace> <account>",
-			Examples:         []string{"mynamespace myaccount"},
 		},
 	}
 }
